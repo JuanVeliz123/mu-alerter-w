3adf0291927e7d4076213de9bbbb6bcdecfbbfde
JohnV <velizjuanagustin@gmail.com>
2020-03-31 21:46:19 -0300

    Added alert functionality

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect, useState } from 'react';
+import { remote } from 'electron';
 import styles from './Home.css';
 import { getUpdateEventTimes } from '../utils/DateUtils';
 
 export default function Home() {
+  // eslint-disable-next-line spaced-comment
+  //#region State Definition
   const [cpEventDate, setCpEventDate] = useState('');
+  const [shouldAlertCp, setShouldAlertCp] = useState(true);
   const [mossDate, setMossDate] = useState('');
   const [balrogDate, setBalrogDate] = useState('');
   const [metalBalrogDate, setMetalBalrogDate] = useState('');
@@ -21,6 +26,8 @@ export default function Home() {
   const [abyssalInvDate, setAbyssalInvDate] = useState('');
   const [medusaInvDate, setMedusaInvDate] = useState('');
   const [gorgonInvDate, setGorgonInvDate] = useState('');
+  // eslint-disable-next-line spaced-comment
+  //#endregion
 
   const updateEventTimes = () => {
     const times = getUpdateEventTimes();
@@ -42,8 +49,42 @@ export default function Home() {
     setDarkSwampInvDate(times.filter(x => x.name === 'DarkSwamp')[0].countdown);
     setKuberaMineInvdate(times.filter(x => x.name === 'Abyssal')[0].countdown);
     setAbyssalInvDate(times.filter(x => x.name === 'DarkSwamp')[0].countdown);
-    setGorgonInvDate(times.filter(x => x.name === 'DarkSwamp')[0].countdown);
-    setMedusaInvDate(times.filter(x => x.name === 'Gorgon')[0].countdown);
+    setGorgonInvDate(times.filter(x => x.name === 'Gorgon')[0].countdown);
+    setMedusaInvDate(times.filter(x => x.name === 'Medusa')[0].countdown);
+  };
+
+  const showWindowOnTop = () => {
+    const window = remote.getCurrentWindow();
+    if (window.isAlwaysOnTop()) {
+      return;
+    }
+    window.setAlwaysOnTop(true, 'status');
+    window.show();
+    setTimeout(() => {
+      window.setAlwaysOnTop(false);
+    }, 300);
+  };
+
+  const getCounterStyles = (eventDate: string) => {
+    const splitted = eventDate.split(':');
+    if (splitted[0] === '0' && splitted[1] === '00') {
+      return { className: styles.counter, style: { color: 'yellow' } };
+    }
+    return { className: styles.counter };
+  };
+
+  const popWindowIfNecessary = () => {
+    if (!shouldAlertCp) {
+      return;
+    }
+    const splitted = cpEventDate.split(':');
+    if (
+      splitted[0] === '0' &&
+      splitted[1] === '00' &&
+      parseInt(splitted[2], 10) <= 10
+    ) {
+      showWindowOnTop();
+    }
   };
 
   useEffect(() => {
@@ -54,83 +95,95 @@ export default function Home() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    popWindowIfNecessary();
+  }, [cpEventDate]);
+
   return (
     <div className={styles.container} data-tid="container">
       <h1>Mu Alerter</h1>
       <div className={styles.lists}>
         <div className={styles.listRow}>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(cpEventDate)}>
             <h3>CP EVENT</h3>
             <span>{cpEventDate}</span>
+            <span style={{ marginLeft: 10 }}>
+              Alert?
+              <input
+                type="checkbox"
+                checked={shouldAlertCp}
+                onChange={() => setShouldAlertCp(!shouldAlertCp)}
+              />
+            </span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(mossDate)}>
             <h3>Moss Gambler</h3>
             <span>{mossDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(balrogDate)}>
             <h3>Balrog</h3>
             <span>{balrogDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(metalBalrogDate)}>
             <h3>Metal Balrog</h3>
             <span>{metalBalrogDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(iceQueenDate)}>
             <h3>Ice Queen</h3>
             <span>{iceQueenDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(hydraDate)}>
             <h3>Hydra</h3>
             <span>{hydraDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(fereaInvDate)}>
             <h3>Ferea invasion</h3>
             <span>{fereaInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(kuberaMineInvDate)}>
             <h3>Kubera Mine Invasion</h3>
             <span>{kuberaMineInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(gorgonInvDate)}>
             <h3>Gorgon invasion</h3>
             <span>{gorgonInvDate}</span>
           </div>
         </div>
 
-        <div className={styles.listRow}>
+        <div {...getCounterStyles(canyonInvDate)}>
           <div className={styles.counter}>
             <h3>Canyon Invasion</h3>
             <span>{canyonInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(undinesInvDate)}>
             <h3>Undines Invasion</h3>
             <span>{undinesInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(debenterInvDate)}>
             <h3>Debenter Invasion</h3>
             <span>{debenterInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(urukNarsInvDate)}>
             <h3>Uruk/Nars Invasion</h3>
             <span>{urukNarsInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(nixInvDate)}>
             <h3>Nix Lake Invasion</h3>
             <span>{nixInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(deepDungInvDate)}>
             <h3>Deep Dung Invasion</h3>
             <span>{deepDungInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(darkSwampInvDate)}>
             <h3>Dark Swamp Invasion</h3>
             <span>{darkSwampInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(abyssalInvDate)}>
             <h3>Abyssal Invasion</h3>
             <span>{abyssalInvDate}</span>
           </div>
-          <div className={styles.counter}>
+          <div {...getCounterStyles(medusaInvDate)}>
             <h3>Medusa Invasion</h3>
             <span>{medusaInvDate}</span>
           </div>
